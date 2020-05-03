9adc75e8d6a6b478b591ee565880f817c441f338
Ganeshk750 <ganesh9ot@gmail.com>
2020-05-04 00:02:15 +0530

    adding put and delete method in controller

--- ReactiveApplication/src/main/java/com/ganesh/controller/ReservationResource.java
+++ ReactiveApplication/src/main/java/com/ganesh/controller/ReservationResource.java
@@ -2,9 +2,11 @@ package com.ganesh.controller;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +34,17 @@ public class ReservationResource {
 		
 		return Mono.just("{}");
 		
+	}
+	
+	@PutMapping(path="{roomId}", produces=MediaType.APPLICATION_JSON_UTF8_VALUE, consumes=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public Mono<String> updatePrice(@PathVariable String roomId, @RequestBody Mono<Reservation> reservation) {
+		
+		return Mono.just("{}");
+	}
+	
+	@DeleteMapping(path="{roomId}")
+	public Mono<Boolean> deleteReservation(@PathVariable String roomId){
+		return Mono.just(true);
 	}
 	
 	
