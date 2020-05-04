cfcc43eca01ff087a149f52b2f52d0c48dcc4f10
Ganeshk750 <ganesh9ot@gmail.com>
2020-05-04 14:44:52 +0530

    Imp in ReservationService Method in ReservationController

--- ReactiveApplication/src/main/java/com/ganesh/controller/ReservationResource.java
+++ ReactiveApplication/src/main/java/com/ganesh/controller/ReservationResource.java
@@ -1,5 +1,6 @@
 package com.ganesh.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ganesh.model.Reservation;
+import com.ganesh.service.ReservationService;
 
 import reactor.core.publisher.Mono;
 
@@ -22,29 +24,43 @@ public class ReservationResource {
 
 	public static final String ROOM_V1_RESERVATION = "/room/v1/reservation/";
 	
-	@GetMapping(path="{roomId}", produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public Mono<String> getReservationById(@PathVariable String roomId){
+	private ReservationService reservationService;
+	
+	@Autowired
+	public ReservationResource(ReservationService reservationService) {
+		this.reservationService = reservationService;
+	}
+	
+	@GetMapping(path="{id}", produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public Mono<Reservation> getReservationById(@PathVariable String id){
 		
-		return Mono.just("{}");
+		//return Mono.just("{}");
+		
+		return reservationService.getReservation(id);
 		
 	}
 	
 	@PostMapping(path="", produces=MediaType.APPLICATION_JSON_UTF8_VALUE, consumes=MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public Mono<String> createReservation(@RequestBody Mono<Reservation> reservation){
+	public Mono<Reservation> createReservation(@RequestBody Mono<Reservation> reservation){
 		
-		return Mono.just("{}");
+		//return Mono.just("{}");
+		
+		return reservationService.createReservation(reservation);
 		
 	}
 	
-	@PutMapping(path="{roomId}", produces=MediaType.APPLICATION_JSON_UTF8_VALUE, consumes=MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public Mono<String> updatePrice(@PathVariable String roomId, @RequestBody Mono<Reservation> reservation) {
+	@PutMapping(path="{id}", produces=MediaType.APPLICATION_JSON_UTF8_VALUE, consumes=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public Mono<Reservation> updatePrice(@PathVariable String id, @RequestBody Mono<Reservation> reservation) {
 		
-		return Mono.just("{}");
+		//return Mono.just("{}");
+		return reservationService.updateReservation(id, reservation);
 	}
 	
-	@DeleteMapping(path="{roomId}")
-	public Mono<Boolean> deleteReservation(@PathVariable String roomId){
-		return Mono.just(true);
+	@DeleteMapping(path="{id}")
+	public Mono<Boolean> deleteReservation(@PathVariable String id){
+		//return Mono.just(true);
+		
+		return reservationService.deleteReservation(id);
 	}
 	
 	
